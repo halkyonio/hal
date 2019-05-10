8e9a9072e13f8c00a93ce1e8a34f51b9ae9c49f8
Chris Laprun <metacosm@gmail.com>
2019-05-10 22:18:31 +0200

    feat: try to implement implicit call to init when pushing without it. DOES NOT WORK since it needs to wait for the component to be ready *AND* calling k8s commands directly doesn't work because k8s codebase imports dependencies with broken modules.

--- pkg/kreate/cli/push/push.go
+++ pkg/kreate/cli/push/push.go
@@ -5,10 +5,11 @@ import (
 	"fmt"
 	"github.com/snowdrop/kreate/pkg/cmdutil"
 	"github.com/snowdrop/kreate/pkg/k8s"
+	"github.com/snowdrop/kreate/pkg/log"
 	"github.com/spf13/cobra"
 	"io"
+	"k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"os/exec"
 	"path/filepath"
 )
 
@@ -28,15 +29,34 @@ func (o *options) Validate() error {
 
 func (o *options) Run() error {
 	c := k8s.GetClient()
+	selector := "app=" + o.TargetName
 	pods, err := c.KubeClient.CoreV1().Pods(c.Namespace).List(metav1.ListOptions{
-		LabelSelector: "app=" + o.TargetName,
+		LabelSelector: selector,
 		Limit:         1,
 	})
 	if err != nil {
 		return err
 	}
 
-	podName := pods.Items[0].Name
+	var podName string
+	if len(pods.Items) == 0 {
+		// the pod doesn't exist, create it and wait for it to be ready
+		component := filepath.Join(o.TargetPath, "target", "classes", "META-INF", "ap4k", "component.yml")
+
+		err := k8s.Apply(component, c.Namespace)
+		if err != nil {
+			return fmt.Errorf("error applying component CR: %v", err)
+		}
+
+		pod, err := c.WaitAndGetPod(selector, v1.PodRunning, "Component for "+o.TargetName+" initialized. Waiting for it to be ready…")
+		if err != nil {
+			return fmt.Errorf("error waiting for pod: %v", err)
+		}
+
+		podName = pod.Name
+	} else {
+		podName = pods.Items[0].Name
+	}
 
 	/*// todo: fix copy function
 	err = c.CopyFile(".", podName, "/deployments", []string{"target/" + app + "-0.0.1-SNAPSHOT.jar"}, nil)
@@ -45,11 +65,13 @@ func (o *options) Run() error {
 	}*/
 
 	jar := filepath.Join(o.TargetPath, "target", o.TargetName+"-0.0.1-SNAPSHOT.jar")
-	command := exec.Command("kubectl", "cp", jar, fmt.Sprintf("%s:/deployments/app.jar", podName), "-n", c.Namespace)
-	err = command.Run()
+	s := log.Spinner("Uploading " + jar)
+	defer s.End(false)
+	err = k8s.Copy(jar, c.Namespace, podName)
 	if err != nil {
-		return err
+		return fmt.Errorf("error uploading jar: %v", err)
 	}
+	s.End(true)
 
 	// use pipes to write output from ExecCMDInContainer in yellow  to 'out' io.Writer
 	pipeReader, pipeWriter := io.Pipe()
