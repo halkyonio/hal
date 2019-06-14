9c3ce31e584eefc9df0b8873536aeb9f23931ae6
Chris Laprun <metacosm@gmail.com>
2019-06-14 22:55:59 +0200

    fix: make NewEnumValue work with strings as well

--- pkg/validation/enumvalue.go
+++ pkg/validation/enumvalue.go
@@ -15,11 +15,15 @@ type EnumValue struct {
 func (e EnumValue) String() string {
 	return e.Value
 }
-func NewEnumValue(valueName string, values ...fmt.Stringer) EnumValue {
+func NewEnumValue(valueName string, values ...interface{}) EnumValue {
 	if len(values) > 0 {
 		valueMap := make(map[string]bool, len(values))
 		for _, value := range values {
-			valueMap[value.String()] = true
+			s, err := valueAsString(value)
+			if err != nil {
+				panic(err)
+			}
+			valueMap[s] = true
 		}
 		set := EnumValue{
 			values:    valueMap,
@@ -31,20 +35,21 @@ func NewEnumValue(valueName string, values ...fmt.Stringer) EnumValue {
 	panic(fmt.Errorf("a EnumValue must contain at least one possible value"))
 }
 func (e EnumValue) Contains(ans interface{}) error {
-	var value string
-	switch v := ans.(type) {
-	case fmt.Stringer:
-		value = v.String()
-	case string:
-		value = v
-	default:
-		return fmt.Errorf("can only validate string or Stringer instances, was given: %v", ans)
-	}
-
-	if !e.values[value] {
+	if value, err := valueAsString(ans); err != nil || !e.values[value] {
 		return fmt.Errorf("unknown %s: %s, valid %ss are: %s", e.valueName, value, e.valueName, e.knownValues)
 	}
 	return nil
+}
+
+func valueAsString(ans interface{}) (string, error) {
+	switch v := ans.(type) {
+	case fmt.Stringer:
+		return v.String(), nil
+	case string:
+		return v, nil
+	default:
+		return "", fmt.Errorf("can only validate string or Stringer instances, was given: %v", ans)
+	}
 }
 func (e *EnumValue) GetKnownValues() string {
 	if len(e.knownValues) == 0 {
