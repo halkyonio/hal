c4b004fc8d43c79fe38e81f14ff1fc99bc183252
aurea munoz <amunozhe@redhat.com>
2019-09-13 12:48:32 +0200

    doc: add hal cmd Example

--- pkg/hal/cli/hal.go
+++ pkg/hal/cli/hal.go
@@ -7,9 +7,15 @@ import (
 	"halkyon.io/hal/pkg/hal/cli/component"
 	"halkyon.io/hal/pkg/hal/cli/link"
 	"halkyon.io/hal/pkg/hal/cli/version"
+	ktemplates "k8s.io/kubectl/pkg/util/templates"
 )
 
 const commandName = "hal"
+
+var (
+	halExample = ktemplates.Examples(`  # Displays hal help
+ %[1]s  --help`)
+)
 
 func NewCmdHal() *cobra.Command {
 	hal := &cobra.Command{
@@ -17,6 +23,7 @@ func NewCmdHal() *cobra.Command {
 		Short: "Easily create Kubernetes applications",
 		Long: fmt.Sprintf(`%s
 Easily create and manage Kubernetes applications using Dekorate and the Halkyon operator.`, version.Version()),
+		Example: fmt.Sprintf(halExample, commandName),
 	}
 
 	hal.AddCommand(
