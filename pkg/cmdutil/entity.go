c30af972e1ff41ae6be7de5cc8c3339b35cba86e
Chris Laprun <metacosm@gmail.com>
2019-09-17 22:09:23 +0200

    refactor: add HalkyonEntityOptions and use it for DeleteOptions

--- pkg/cmdutil/entity.go
+++ pkg/cmdutil/entity.go
@@ -1,6 +1,23 @@
 package cmdutil
 
-import v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+import (
+	"fmt"
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	ktemplates "k8s.io/kubectl/pkg/util/templates"
+	"strings"
+)
+
+type HalkyonEntityOptions struct {
+	ResourceType string
+	Name         string
+	Client       HalkyonEntity
+}
+
+func (o *HalkyonEntityOptions) genericExample(cmdName, fullParentName string) string {
+	tmpl := ktemplates.Examples(`  # %[1]s the %[2]s named 'foo'
+  %[3]s foo`)
+	return fmt.Sprintf(tmpl, strings.Title(cmdName), o.ResourceType, CommandName(cmdName, fullParentName))
+}
 
 type HalkyonEntity interface {
 	Get(string, v1.GetOptions) error
