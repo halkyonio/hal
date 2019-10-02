f7babe0587e5ac59d8f58ea5d06869e346ef0237
aurea munoz <amunozhe@redhat.com>
2019-10-02 12:24:59 +0200

    chore: enhance script for recording and add link in readme

--- demo/play-demo.sh
+++ demo/play-demo.sh
@@ -1,60 +1,48 @@
 #!/bin/bash
 
-#asciinema rec -w 2 &
+#asciinema rec -w 2
 
 exec() {
-  echo "\$ $@"|pv -qL 20 ; "$@" ;
+  echo "\$ $@"|pv -qL 10 ; "$@" ;
   }
 
 echo "   "
 echo "   "
-echo "  Simplify the deployment of Spring Boot applications using Halkyon Component Operator on Kubernetes"|pv -qL 30
-echo "  In this demo, we will :"|pv -qL 20
-echo "   --> Compose & link microservices"|pv -qL 20
-echo "   --> Deploy a capability such as a database and link it to a microservice consuming it"|pv -qL 20
-echo "   --> Code locally and next push/build on k8s/OpenShift"|pv -qL 20
-echo "  "|pv -qL 20
-echo "  Ready? Let's start!"|pv -qL 20
+echo " Simplify the deployment of Spring Boot applications using Halkyon Component Operator on Kubernetes"|pv -qL 10
+echo " In this demo, we will :"|pv -qL 10
+echo "   --> Compose & link microservices"|pv -qL 10
+echo "   --> Deploy a capability such as a database and link it to a microservice consuming it"|pv -qL 10
+echo "   --> Code locally and next push/build on Kubernetes/OpenShift"|pv -qL 10
+echo "  "|pv -qL 10
+echo " Ready? Let's start!"|pv -qL 10
 echo "  "
 echo "  "
+sleep 5
+
+clear && sleep 1
+echo "# Log on to the cluster using the oc client"|pv -qL 10
+sleep 2
+exec oc login https://159.69.209.188:8443 --token=Hz8JKkb80xa7rptVGJhq6aerZUGwyaRVYJ7zBQfFly8
 sleep 3
 
 clear && sleep 1
-echo "# Log on to the cluster using the oc client"|pv -qL 20
-#exec oc login https://api.cluster-b1c8.b1c8.sandbox941.opentlc.com:6443 -u user1 -p r3dh4t1!
-exec oc login https://api.cluster-6c2c.6c2c.sandbox1254.opentlc.com:6443 -u user1 -p r3dh4t1!
-sleep 3
-
-clear && sleep 1
-echo "# Create a new project"|pv -qL 20
-exec oc new-project demo
-sleep 3
-
-clear && sleep 1
-echo "# Create a directory named demo and subsequently enter in"|pv -qL 20
-exec mkdir demo
-exec cd demo
-sleep 3
-
-clear && sleep 1
-echo "# Create a pom.xml with the following content"|pv -qL 20
+echo "# Create a new project"|pv -qL 10
 sleep 1
+exec oc new-project demo
+sleep 5
+
+clear && sleep 1
+echo "# Create a directory named demo and subsequently enter in"|pv -qL 10
+sleep 2
+exec mkdir demo
+sleep 1
+exec cd demo
+sleep 5
+
+clear && sleep 1
+echo "# Create a pom.xml with the following content"|pv -qL 10
+sleep 2
 echo "<?xml version="1.0" encoding="UTF-8"?>
-<!--
-Copyright 2016-2017 Red Hat, Inc, and individual contributors.
-
- Licensed under the Apache License, Version 2.0 (the "License");
- you may not use this file except in compliance with the License.
- You may obtain a copy of the License at
-
- http://www.apache.org/licenses/LICENSE-2.0
-
- Unless required by applicable law or agreed to in writing, software
- distributed under the License is distributed on an "AS IS" BASIS,
- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- See the License for the specific language governing permissions and
- limitations under the License.
--->
 <project xmlns="http://maven.apache.org/POM/4.0.0" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
          xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 http://maven.apache.org/xsd/maven-4.0.0.xsd">
     <modelVersion>4.0.0</modelVersion>
@@ -69,24 +57,9 @@ Copyright 2016-2017 Red Hat, Inc, and individual contributors.
         <module>fruit-client-sb</module>
     </modules>
 </project>"
-sleep 3
+sleep 10
 cat > pom.xml << ENDOFFILE
 <?xml version="1.0" encoding="UTF-8"?>
-<!--
-Copyright 2016-2017 Red Hat, Inc, and individual contributors.
-
- Licensed under the Apache License, Version 2.0 (the "License");
- you may not use this file except in compliance with the License.
- You may obtain a copy of the License at
-
- http://www.apache.org/licenses/LICENSE-2.0
-
- Unless required by applicable law or agreed to in writing, software
- distributed under the License is distributed on an "AS IS" BASIS,
- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- See the License for the specific language governing permissions and
- limitations under the License.
--->
 <project xmlns="http://maven.apache.org/POM/4.0.0" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
          xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 http://maven.apache.org/xsd/maven-4.0.0.xsd">
     <modelVersion>4.0.0</modelVersion>
@@ -103,78 +76,132 @@ Copyright 2016-2017 Red Hat, Inc, and individual contributors.
 </project>
 ENDOFFILE
 clear && sleep 1
-echo "# Create a new client project using the REST HTTP client template proposed by the scaffolding tool"|pv -qL 20
-exec hal component spring-boot -i fruit-client-sb -g me.fruitstand -p me.fruitstand.demo -s 2.1.6.RELEASE -t client -v 1.0.0-SNAPSHOT --supported=false  fruit-client-sb
+echo "# Create a new client project using the REST HTTP client template proposed by the scaffolding tool"|pv -qL 10
+sleep 3
+exec hal component spring-boot \
+   -i fruit-client-sb \
+   -g me.fruitstand \
+   -p me.fruitstand.demo \
+   -s 2.1.6.RELEASE \
+   -t client \
+   -v 1.0.0-SNAPSHOT \
+   --supported=false  \
+   fruit-client-sb
 sleep 3
 
 clear && sleep 1
-echo "# Create a backend project interactively and use as template the crud type and fruit-backend-sb as maven project name"|pv -qL 20
-exec hal component spring-boot  fruit-backend-sb
+echo "# Create a backend project interactively and use as template the crud type and fruit-backend-sb as maven project name"|pv -qL 10
+sleep 3
+exec hal component spring-boot fruit-backend-sb
 sleep 3
 
 clear && sleep 1
-echo "# Build the projects"|pv -qL 20
-echo "# Compile and generate the uber jar of the Spring Boot application client"|pv -qL 20
+echo "# Build the projects"|pv -qL 10
+echo "# Compile and generate the uber jar of the Spring Boot application client"|pv -qL 10
+sleep 3
 exec mvn package -f fruit-client-sb
 sleep 3
 
 clear && sleep 1
-echo "# Repeat the command executed previously for the CRUD - backend microservice."|pv -qL 20
-echo "# We need to use the kubernetes profile because the project is set up to work both locally using H2 database for quick testing and "remotely" using a PostgreSQL database."|pv -qL 20
+echo "# Repeat the command executed previously for the CRUD - backend microservice."|pv -qL 10
+echo "# We need to use the kubernetes profile because the project is set up to work both locally using H2 database for quick testing and "remotely" using a PostgreSQL database."|pv -qL 10
+sleep 4
 exec mvn package -f fruit-backend-sb -Pkubernetes
 sleep 3
 
 clear && sleep 1
-echo "# Deploy the applications as components"|pv -qL 20
-exec hal component push -c fruit-client-sb,fruit-backend-sb
+echo "# Deploy the applications as components"|pv -qL 10
+sleep 2
+exec hal component create -c fruit-client-sb
+sleep 2
+exec hal component create -c fruit-backend-sb
 sleep 3
 
 clear && sleep 1
-echo "# Check if the components have been correctly installed"|pv -qL 20
+echo "# Check if the components have been correctly installed"|pv -qL 10
+sleep 2
 exec oc get cp
 sleep 3
 
 clear && sleep 1
-echo "# Create a capability to install a PostgreSQL database using the interactive mode"|pv -qL 20
-exec hal capability create
+echo "# Create a capability to install a PostgreSQL database using the interactive mode"|pv -qL 10
+sleep 2
+exec hal capability create -n postgres-db
 sleep 3
 
 clear && sleep 1
-echo "# Check the capability status"|pv -qL 20
+echo "# Check the capability status"|pv -qL 10
+sleep 2
 exec oc get capabilities
+sleep 10
+
+clear && sleep 1
+echo "# Link the microservices"|pv -qL 10
+echo "# We need to wire the fruit-backend-sb component with the postgres-db capability by creating a link between both"|pv -qL 10
+sleep 4
+exec hal link create -n backend-to-db -t fruit-backend-sb
 sleep 3
 
 clear && sleep 1
-echo "# Link the microservices"|pv -qL 20
-echo "# We need to wire the fruit-backend-sb component with the postgres-db capability by creating a link between both"|pv -qL 20
-exec hal link create
+echo "# Now, create a link targeting the fruit-client-sb component to wire the client and backend"|pv -qL 10
+sleep 3
+exec hal link create -n client-to-backend -t fruit-client-sb -e KUBERNETES_ENDPOINT_FRUIT=http://fruit-backend-sb:8080/api/fruits
 sleep 3
 
 clear && sleep 1
-echo "# Now, create a link targeting the fruit-client-sb component to wire the client and backend"|pv -qL 20
-exec hal link create
-sleep 3
-
-clear && sleep 1
-echo "# Check the link status"|pv -qL 20
+echo "# Check the link status"|pv -qL 10
+sleep 2
 exec oc get links
 sleep 3
 
 clear && sleep 1
-echo "# Try the backend service to see if it works"|pv -qL 20
-echo "# So, get the route address of the backend microservice using this command "|pv -qL 20
+echo "# Push the code"|pv -qL 10
+sleep 2
+exec hal component push -c fruit-client-sb,fruit-backend-sb
+echo "# Let's wait some seconds in order the applications are readies to respond"|pv -qL 10
+#Wait some seconds for pods readies
+i=0
+while [ $i -le 15 ]
+do
+    printf '. '
+    i=$(( $i + 1 ))
+    sleep 1
+done
+
+
+clear && sleep 1
+echo "# Try the backend service to see if it works"|pv -qL 10
+echo "# So, get the route address of the backend microservice using this command "|pv -qL 10
+sleep 4
 exec oc get routes/fruit-backend-sb --template={{.spec.host}}
 echo " "
 sleep 2
-echo "# Copy/paste the address displayed within the terminal in a browser and create some fruits ;-)"|pv -qL 20
+echo "# Let's create some fruits ;-)"|pv -qL 10
+BACKEND_URL=$(oc get routes/fruit-backend-sb --template={{.spec.host}})
+exec http -s solarized POST "http://${BACKEND_URL}/api/fruits" name=Orange
+sleep 1
+exec http -s solarized POST "http://${BACKEND_URL}/api/fruits" name=Banana
+sleep 1
+exec http -s solarized POST "http://${BACKEND_URL}/api/fruits" name=Pineapple
 sleep 10
 
 clear && sleep 1
-echo "# Try the client microservice to see if it works too"|pv -qL 20
-echo "# So, get also its route address "|pv -qL 20
+echo "# Try the client microservice to see if it works too"|pv -qL 10
+echo "# So, get also its route address "|pv -qL 10
+sleep 4
 exec oc get routes/fruit-client-sb --template={{.spec.host}}
 sleep 3
 
 clear && sleep 1
-echo "# curl the service within your terminal, you should get the fruits created in the previous step."|pv -qL 20
-exec curl "http://$(oc get routes/fruit-client-sb --template={{.spec.host}})/api/client"
+echo "# curl the service within your terminal, you should get the fruits created in the previous step."|pv -qL 10
+exec http "http://$(oc get routes/fruit-client-sb --template={{.spec.host}})/api/client"
+sleep 2
+echo " "
+echo " "
+echo "# So, this is hal!! "|pv -qL 10
+echo "# Thank you :-) "|pv -qL 10
+clear
+
+# clean up
+oc delete project demo
+cd .. && rm -rf demo
